865ed0b75cb0320548f75968c28a8cf8d090aeac
Binyam Mulat Abegaz <bini.blue.1996@gmail.com>
2025-03-07 01:28:03 +0300

    added protected route to router

--- app/router/routers.py
+++ app/router/routers.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 from app.router.endpoints.authRouter import authRouter
 from app.router.endpoints.userRouter import userRouter, rootRouter, instructorRouter
-from app.router.endpoints.courseRouter import courseRouter
+from app.router.endpoints.courseRouter import courseRouter, protected_courseRouter
 
 routers = APIRouter()
 
-routerList = [authRouter, userRouter, rootRouter, courseRouter, instructorRouter]
+routerList = [authRouter, userRouter, rootRouter, courseRouter, instructorRouter, protected_courseRouter]
 
 for router in routerList:
     routers.include_router(router)
